7b0f1cfee426070c4f61547a08358445a4178574
Nikhil Rane <nikhilrane1992@gmail.com>
2017-09-26 12:47:20 +0530

    added github calender, added company experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,11 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "nikhilrane1992");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "nikhilrane1992", selector: "#ghfeed" });
+    GitHubCalendar(".github-graph", "nikhilrane1992");
 
 
 });
